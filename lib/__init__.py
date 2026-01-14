effb409a50187286f3759c4fe87545986388acf6
saahilbijlani-tracksuit <saahil.bijlani@gotracksuit.com>
2026-01-14 12:44:44 +0700

    Simplify lib/__init__.py to avoid circular imports

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,18 +1,1 @@
-"""
-HubSpot Signal Matcher Library
-
-Core modules for matching HubSpot Signals to Companies and Contacts
-using semantic embeddings and vector similarity search.
-"""
-
-from .hubspot_client import HubSpotClient
-from .supabase_client import SupabaseClient
-from .embeddings import EmbeddingGenerator
-from .matcher import SignalMatcher
-
-__all__ = [
-    "HubSpotClient",
-    "SupabaseClient", 
-    "EmbeddingGenerator",
-    "SignalMatcher",
-]
+"""HubSpot Signal Matcher Library"""
